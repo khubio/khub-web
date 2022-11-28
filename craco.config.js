3ea7f6fc3f89299b83805acedf2bb8626618935d
trinhvnguyen <vantrinh.fit@gmail.com>
2022-11-28 23:22:26 +0700

    send mail when register

--- craco.config.js
+++ craco.config.js
@@ -14,6 +14,7 @@ module.exports = {
       '@services': path.resolve(__dirname, 'src/services'),
       '@configs': path.resolve(__dirname, 'src/configs'),
       '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
     },
   },
 };
